40773a436172053a1b7de13134fb0dd264d72ad4
qmspc <qmspc4@outlook.com>
2024-12-11 15:46:34 -0600

    test

--- load_data.m
+++ load_data.m
@@ -1,31 +1,8 @@
 clear; clc; close all
 
-data_path = "data/2024/12 December/20241203/0837-MOT loading";
+data_path = "data\2024\12 December\20241211\0333-";
 files = dir(data_path + "/images/*.csv");
 
-Data = struct('IMG2', [], 'IMG3', [], 'IMG4', []);
-
-for i = 1: length(files)
-    f = files(i);
-    if contains(f.name, 'IMG2')
-        category = "IMG2";
-    elseif contains(f.name, 'IMG3')
-        category = "IMG3";
-    elseif contains(f.name, 'IMG4')
-        category = "IMG4";
-    elseif contains(f.name, 'OD')
-        category = "OD";
-    else
-        warning('Unable to categorize image!')
-    end
-    if category == "OD"
-    else
-        params = sscanf(f.name, "%d-" + category + "-%f-%f-%f.csv");
-        shot = struct('data', readmatrix([f.folder, '/', f.name]), 'scan1', params(2), ...
-            'scan2', params(1), 'scan3', params(3), 'scan4', params(4));
-        Data.(category) = [Data.(category), shot];
-    end
-end
-
 %%
-imagesc2(Data.IMG2(1).data)
+data = readmatrix(['data\2024\12 December\20241211\0333-', '/images/', '0-IMG4-2.420000-3.200000--0.900000.csv']);
+imagesc2(data)
